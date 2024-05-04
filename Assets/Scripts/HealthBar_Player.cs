aabd5fe3350370a5fa4237cd19780569cdff7d6b
Milay <120564668+KMinhMilay@users.noreply.github.com>
2024-05-05 06:55:49 +0700

    createHealthBar

---
+++ Assets/Scripts/HealthBar_Player.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float health;
+    float maxHealth=100;
+    public Image healthBar;
+    float lerpSpeed;
+    void Start()
+    {
+        
+        health=maxHealth;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(health>maxHealth) health = maxHealth;
+        lerpSpeed = 3f * Time.deltaTime;
+        HealthBarFiller();
+    }
+    void HealthBarFiller()
+    {
+        healthBar.fillAmount = Mathf.Lerp(healthBar.fillAmount,health/maxHealth,lerpSpeed);
+    }
+    public void Damage(float damagePoints)
+    {
+        if(health > 0)
+        {
+            health -= damagePoints;
+        }
+    }
+    public void Heal(float healingPoints)
+    {
+        if (health < maxHealth)
+        {
+            health += healingPoints;
+        }
+    }
+}
