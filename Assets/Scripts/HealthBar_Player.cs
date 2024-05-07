08f1d12fa5dce25cca5bf5a90d3ca856455b54d5
Milay <120564668+KMinhMilay@users.noreply.github.com>
2024-05-07 11:49:59 +0700

    update final

--- Assets/Scripts/HealthBar_Player.cs
+++ Assets/Scripts/HealthBar_Player.cs
@@ -34,7 +34,7 @@ public class NewBehaviourScript : MonoBehaviour
     {
         if(health > 0)
         {
-            Console.WriteLine("Đã nhận damage. Trừ máu");
+            print("Đã nhận damage. Trừ máu");
             health -= damagePoints;
         }
     }
@@ -42,7 +42,7 @@ public class NewBehaviourScript : MonoBehaviour
     {
         if (health < maxHealth)
         {
-            Console.WriteLine("Đã phục hồi máu");
+            print("Đã phục hồi máu");
             health += healingPoints;
         }
     }
